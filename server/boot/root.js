da4c4aeb6d6cd7cd9fb61597b66ca1a9ca3399b6
cschweda <cschweda@gmail.com>
2017-07-14 09:56:31 -0500

    Custom homepage / status route

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,11 @@
 'use strict';
-
+var path = require('path');
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/status', server.loopback.status());
+  router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../../client/index.html'));
+  });
   server.use(router);
 };
